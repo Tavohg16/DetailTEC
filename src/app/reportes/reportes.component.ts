21cf5938e000508fd29047c7060b6d1c9bc360ba
Jose Julian <juliansolqu18@gmail.com>
2022-10-18 22:52:30 -0600

    [P1-33] Agrega gestión de reportes

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -3,47 +3,51 @@ import { Router } from '@angular/router';
 import { ReportesService } from '../services/reportes/reportes.service';
 import { ReporteResponse } from '../services/reportes/reportes.types';
 
+
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
   styleUrls: ['./reportes.component.css'],
 })
 export class ReportesComponent implements OnInit {
-  protected options = [
-    {
-      title: 'Planilla',
-      descripcion:
-        'Listado del pago a los empleados.',
-      icon: 'fa-solid fa-money-bill',
-      id: 1,
-    },
-    {
-      title: 'Tipos de lavado por cliente',
-      descripcion:
-        'Listado de lavados realizados por el cliente y la cantidad correspondiente a cada lavado.',
-      icon: 'fa-solid fa-person',
-      id: 2,
-    },
-    {
-      title: 'Redencion de puntos',
-      descripcion: 'Listado de los clientes que más puntos han redimido.',
-      icon: 'fa-solid fa-medal',
-      id: 3,
-    },
-  ];
+    cedula: any;
+    protected options = [
+        {
+            title: 'Planilla',
+            descripcion:
+            'Listado del pago a los empleados.',
+            icon: 'fa-solid fa-money-bill',
+            id: 1,
+        },
+        {
+            title: 'Tipos de lavado por cliente',
+            descripcion:
+            'Listado de lavados realizados por el cliente y la cantidad correspondiente a cada lavado.',
+            icon: 'fa-solid fa-person',
+            id: 2,
+        },
+        {
+            title: 'Redencion de puntos',
+            descripcion: 'Listado de los clientes que más puntos han redimido.',
+            icon: 'fa-solid fa-medal',
+            id: 3,
+        },
+    ];
 
-  constructor(private reportesService: ReportesService, private router: Router) {
-    
+  constructor(
+    private reportesService: ReportesService, 
+    private router: Router
+    ) {
   }
 
   ngOnInit(): void {}
 
   protected generarReporte(reporte: any) {
     let reporteFormato: any;
-    if (reporte.id === 2) {
+    if (reporte.id == 2) {
       reporteFormato = {
         tipo_reporte: reporte.id,
-        //cedula_cliente: poner cedula de cliente obtenida desde field aqui
+        cedula_cliente: this.cedula,
       };
     } else {
       reporteFormato = {
